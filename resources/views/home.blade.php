7d93cc8903ff58c24b195d3eeb39491917d554cb
Anan1711 <60573839+Anan1711@users.noreply.github.com>
2020-06-28 11:36:55 +0600

    Model Relationship
    Created a relation between user and post

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,6 +16,33 @@
                    <a href="/posts/create" class = "btn btn-primary">Create Post</a>
                    <p>
                    <h3>Your Blog Posts<h3>
+                    @if(count($posts) > 0)   
+                   <table class="table table-striped">
+                       <tr>
+                           <th>Title</th>
+                           <th></th>
+                           <th></th>
+                        <tr>
+                        @foreach ($posts as $post)
+                        <tr>
+                            <td>{{$post->title}}</td>
+                            <td><a href="/posts/{{$post->id}}/edit" class="btn btn-outline-secondary">Edit</a></td>
+                            <td>
+                                {!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'float-right'])!!}
+                                    {{Form::hidden('_method','DELETE')}}
+                                    {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
+                        
+                                {!!Form::close()!!}
+
+                            </td>
+                         <tr>
+                            
+                        @endforeach                      
+                   </table>
+                   @else
+                     
+                     <small><p>You have no posts!</p></small>
+                   @endif
                 </div>
             </div>
         </div>
